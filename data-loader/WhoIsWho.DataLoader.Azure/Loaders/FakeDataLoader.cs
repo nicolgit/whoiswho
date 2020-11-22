1eefbab8772b23e1c17f15c906f29821429850de
NicolD PC <nicolgit@users.noreply.github.com>
2020-11-22 22:53:28 +0100

    fake subscriptions ok

--- data-loader/WhoIsWho.DataLoader.Azure/Loaders/FakeDataLoader.cs
+++ data-loader/WhoIsWho.DataLoader.Azure/Loaders/FakeDataLoader.cs
@@ -48,6 +48,7 @@ namespace WhoIsWho.DataLoader.Azure.Loaders
             await LoadUsersAsync();
             await LoadTags();
             await LoadGroups();
+            await LoadSubscriptions();
         }
 
         private void LoadRBACRoles()
@@ -176,6 +177,88 @@ namespace WhoIsWho.DataLoader.Azure.Loaders
 
                     uig = await InsertOrMergeEntityAsync(uig);
                     logger.LogInformation($"User In Group {uig.ToString()}");
+                }
+            }
+        }
+
+        private async Task LoadSubscriptions()
+        {
+            var rand = new Bogus.Randomizer();
+
+            var fakeSubscriptions= new Faker<WhoIsWhoEntity>()
+                .StrictMode(false)
+                .RuleFor(o => o.PartitionKey, f => $"{AzureItemType.Subscription}{f.UniqueIndex % 10}")
+                .RuleFor(o => o.RowKey, f => $"{f.UniqueIndex}")
+                .RuleFor(o => o.Name, f => $"subscription {f.Commerce.ProductMaterial()} {f.UniqueIndex}")
+                .RuleFor(o => o.Type, f => $"{AzureItemType.Subscription}")
+                .RuleFor(o=> o.DeepLink, f=>f.Internet.UrlWithPath());
+
+            var fakeUserInSubscription = new Faker<WhoIsWhoEntity>()
+                .StrictMode(false)
+                .RuleFor(o => o.PartitionKey, f => $"{AzureItemType.UserInSubscription}{f.UniqueIndex % 10}")
+                .RuleFor(o => o.RowKey, f => $"{f.UniqueIndex}")
+                .RuleFor(o => o.ChildPartitionKey, f => f.PickRandom(users).PartitionKey)
+                .RuleFor(o => o.ChildRowKey, f => f.PickRandom(users).RowKey)
+                .RuleFor(o => o.Type, f => $"{AzureItemType.UserInSubscription}")
+                .RuleFor(o => o.Name, f => f.PickRandom(roles));
+
+            var fakeGroupInSubscription = new Faker<WhoIsWhoEntity>()
+                .StrictMode(false)
+                .RuleFor(o => o.PartitionKey, f => $"{AzureItemType.GroupInSubscription}{f.UniqueIndex % 10}")
+                .RuleFor(o => o.RowKey, f => $"{f.UniqueIndex}")
+                .RuleFor(o => o.ChildPartitionKey, f => f.PickRandom(groups).PartitionKey)
+                .RuleFor(o => o.ChildRowKey, f => f.PickRandom(groups).RowKey)
+                .RuleFor(o => o.Type, f => $"{AzureItemType.GroupInSubscription}")
+                .RuleFor(o => o.Name, f => f.PickRandom(roles));
+
+             var fakeTagInSubscription = new Faker<WhoIsWhoEntity>()
+                .StrictMode(false)
+                .RuleFor(o => o.PartitionKey, f => $"{AzureItemType.TagInSubscription}{f.UniqueIndex % 10}")
+                .RuleFor(o => o.RowKey, f => $"{f.UniqueIndex}")
+                .RuleFor(o => o.ChildPartitionKey, f => f.PickRandom(tags).PartitionKey)
+                .RuleFor(o => o.ChildRowKey, f => f.PickRandom(tags).RowKey)
+                .RuleFor(o => o.Type, f => $"{AzureItemType.TagInSubscription}");
+
+            int max = QUANTITY;
+            for (int i=0; i<max; i++)
+            {
+                var wiw = fakeSubscriptions.Generate();
+                wiw = await InsertOrMergeEntityAsync(wiw);
+                subscriptions.Add(wiw);
+
+                logger.LogInformation($"Subscription added successfully {wiw.ToString()}");
+
+                int usersInSubscriptions = rand.Int(3,5);
+                for (int j=0; j<usersInSubscriptions; j++)
+                {
+                    var uig = fakeUserInSubscription.Generate();
+                    uig.ParentPartitionKey = wiw.PartitionKey;
+                    uig.ParentRowKey = wiw.RowKey;
+
+                    uig = await InsertOrMergeEntityAsync(uig);
+                    logger.LogInformation($"User In Subscription {uig.ToString()}");
+                }
+
+                int groupsInSubscriptions = rand.Int(3,5);
+                for (int j=0; j<groupsInSubscriptions; j++)
+                {
+                    var uig = fakeGroupInSubscription.Generate();
+                    uig.ParentPartitionKey = wiw.PartitionKey;
+                    uig.ParentRowKey = wiw.RowKey;
+
+                    uig = await InsertOrMergeEntityAsync(uig);
+                    logger.LogInformation($"Group In Subscription {uig.ToString()}");
+                }
+
+                 int tagsInSunscription = rand.Int(3,5);
+                for (int j=0; j<tagsInSunscription; j++)
+                {
+                    var tig = fakeTagInSubscription.Generate();
+                    tig.ParentPartitionKey = wiw.PartitionKey;
+                    tig.ParentRowKey = wiw.RowKey;
+
+                    tig = await InsertOrMergeEntityAsync(tig);
+                    logger.LogInformation($"Tag In Subscription {tig.ToString()}");
                 }
             }
         }
